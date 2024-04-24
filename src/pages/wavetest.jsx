9549182d7554a9a38ab5354810d152dfe0cfe02f
Doeun Kim <doeun.kim@emotion.co.kr>
2024-04-24 13:31:29 +0900

    //

--- src/pages/wavetest.jsx
+++ src/pages/wavetest.jsx
@@ -1,30 +1,30 @@
-function Wavetest() {
-  useEffect(() => {
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
+// function Wavetest() {
+//   useEffect(() => {
+//     const canvas = document.createElement("canvas");
+//     const ctx = canvas.getContext("2d");
 
-    window.addEventListener("resize", init, false);
+//     window.addEventListener("resize", init, false);
 
-    const stageWidth = document.body.clientWidth;
-    const stageHeight = document.body.clientHeight;
-    const init = () => {
-      ctx.width = stageWidth * 2;
-      ctx.height = stageHeight * 2;
-      ctx.scale(2, 2);
-    };
+//     const stageWidth = document.body.clientWidth;
+//     const stageHeight = document.body.clientHeight;
+//     const init = () => {
+//       ctx.width = stageWidth * 2;
+//       ctx.height = stageHeight * 2;
+//       ctx.scale(2, 2);
+//     };
 
-    const animate = (t) => {
-      ctx.clearRect(0, 0, stageWidth, stageHeight);
-      requestAnimationFrame(animate);
-    };
-    init();
-    document.body.appendChild(canvas);
-  }, []);
-  return (
-    <div>
-      <Wave></Wave>
-    </div>
-  );
-}
+//     const animate = (t) => {
+//       ctx.clearRect(0, 0, stageWidth, stageHeight);
+//       requestAnimationFrame(animate);
+//     };
+//     init();
+//     document.body.appendChild(canvas);
+//   }, []);
+//   return (
+//     <div>
+//       <Wave></Wave>
+//     </div>
+//   );
+// }
 
-export default Wavetest;
+// export default Wavetest;
